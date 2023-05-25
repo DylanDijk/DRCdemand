b26e0ad052b69c2dfba25ea7f2927c9b88675fa8
codiewood <codiejcwood@gmail.com>
2023-05-25 16:43:15 +0100

    Add survey data processing

--- analysis/process_data.R
+++ analysis/process_data.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 load(file = "data/Irish.RData")
 
 # Shift dateTime column by one hour
@@ -10,6 +11,20 @@ Irish_adj$extra$toy = as.numeric(format(Irish_adj$extra$dateTime, "%j")) /365
 # make tod between 0 and 1
 # Irish_adj$extra$tod = sin((pi*Irish_adj$extra$tod)/24) 
 # keep tod as it is for now
+
+
+# Format survey data
+Irish_adj$survey <- as_tibble(Irish$survey) %>%
+  readr::type_convert(
+    col_types = list(
+      OWNERSHIP = readr::col_factor(),
+      HEAT.HOME = readr::col_factor(),
+      HEAT.WATER = readr::col_factor(),
+      WINDOWS.doubleglazed = readr::col_factor()
+    )
+  ) %>% # Change variables to factors
+  dplyr::mutate(HOME.whitegoods = as.factor(Irish$survey$HOME.APPLIANCE..White.goods.)) %>% # Rename variable
+  dplyr::select(-c(Code, HOME.APPLIANCE..White.goods.)) # Remove redundant variables
 
 save(Irish_adj, file = "data/Irish_adj.RData")
 
