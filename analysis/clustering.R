2f416cee3885f3a1d92e2d015a7ac9ab7bba8ae9
codiewood <codiejcwood@gmail.com>
2023-05-29 17:53:43 +0100

    Add code + data for new clusterings

--- analysis/clustering.R
+++ analysis/clustering.R
@@ -3,20 +3,31 @@ library(tidyverse)
 # Load data
 load("data/Irish_adj.RData")
 survey <- Irish_adj$survey
-n <- nrow(survey)
+
+load("data/Irish_adj_train.RData")
+indCons_train <- Irish_adj_train$indCons
+survey <- survey %>%
+  mutate(meanDem.train = colMeans(indCons_train))
 
 # Calculate gowers distance
-gowers_dist_mat <- matrix(0, nrow=n, ncol=n)
-for(i in 1:n){
-  gowers_dist_mat[,i] <- gower::gower_dist(survey[i,-1],survey[,-1])
+gowers_distance <- function(data){
+  n <- nrow(data)
+  gowers_dist_mat <- matrix(0, nrow=n, ncol=n)
+  for(i in 1:n){
+    gowers_dist_mat[,i] <- gower::gower_dist(data[i,],data[,])
+  }
+  gowers_dist <- as.dist(gowers_dist_mat)
+  return(gowers_dist)
 }
-gowers_dist <- as.dist(gowers_dist_mat)
 
 # Hierarchical clustering using complete linkage
-hc <- hclust(gowers_dist, method="complete")
+hc_all <- hclust(gowers_distance(survey[,-c(1,2)]), method="complete")
+hc_fixed <- hclust(gowers_distance(survey[,-c(1,2,12)]), method="complete")
 
-# Generate data frame with clusering IDs
-hc4 <- data.frame(ID = survey$ID,
-                  cluster = as.factor(cutree(hc, k = 4)))
-
-save(hc4, file = "data/hclust4.RData")
+# Generate data frames with clustering IDs
+for(num_clust in 2^(0:9)){
+  hc <- data.frame(ID = survey$ID,
+                   cluster_all = as.factor(cutree(hc_all, k = num_clust)),
+                   cluster_fixed = as.factor(cutree(hc_fixed, k = num_clust)))
+  save(hc, file = paste0("data/clusters/hclust",num_clust,".Rdata"))
+}
