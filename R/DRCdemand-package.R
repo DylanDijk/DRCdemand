a22a551d06db5db74b3a89c258b093f0abb2731b
codiewood <codiejcwood@gmail.com>
2023-06-02 12:41:19 +0100

    Fix imports

--- R/DRCdemand-package.R
+++ R/DRCdemand-package.R
@@ -2,9 +2,17 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import Rcpp
 #' @import methods
+#' @import Rcpp
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
 #' @importFrom data.table .SD
+#' @importFrom data.table data.table
 #' @importFrom rstan sampling
 #' @useDynLib DRCdemand, .registration = TRUE
 ## usethis namespace: end
