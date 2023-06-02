138bce1be67a9a1b8dbe2e1a98910f69e17b3b93
codiewood <codiejcwood@gmail.com>
2023-06-02 10:47:43 +0100

    Fix attempt for .SD

--- R/forecast.R
+++ R/forecast.R
@@ -28,10 +28,10 @@ clustersum <- function(training, testing, clustdat, clustcol){
 
   # Data Frame with each cluster with summed electricity demand
   traindt <- data.table::data.table(merged_train)
-  sumtrain_dt <- traindt[, lapply(data.table::.SD, sum), by = 'cluster']
+  sumtrain_dt <- traindt[, lapply(.SD, sum), by = 'cluster']
 
   testdt <- data.table::data.table(merged_test)
-  sumtest_dt <- testdt[, lapply(data.table::.SD, sum), by = 'cluster']
+  sumtest_dt <- testdt[, lapply(.SD, sum), by = 'cluster']
 
   extratest$dow <- forcats::fct_collapse(extratest$dow,
                                 Weekend = c('Sun', 'Sat'),
